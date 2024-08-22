8e72d6ec7cef23fc93af25badf7012aac5b95da7
PRIY MAVANI <priy.mavani.cg@gmail.com>
2024-08-22 11:54:09 +0530

    first commit

---
+++ array.js
@@ -0,0 +1,52 @@
+var array = [2,3,["784","010"],"priy","jay"];
+
+// length og array
+console.log(array);
+
+// to know the vale of array
+console.log(array.at(2));
+console.log(array[2])
+
+// to convert array to string
+console.log(array.toString()); //  string separated by comma
+console.log(array.join("*"));//separated by any key 
+
+//to remove last element
+array.pop();
+console.log(array)
+
+console.log(array.pop()); //to print only last element
+
+//to add element at last
+array.push("darshan");
+console.log(array)
+
+console.log(array.push()); //to know length
+
+//remove 1st element and shifts all element to lower index
+array.shift()
+console.log(array)
+
+console.log(array.shift()); // print first element
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
